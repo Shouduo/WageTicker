6cd2d9b51c276c4e5ad3715643a06778b6de9d89
Shouduo <handsomesaywhynot@gmail.com>
2023-06-23 20:46:56 +0800

    Initial Commit

---
+++ WageTicker/WageTickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WageTickerApp.swift
+//  WageTicker
+//
+//  Created by Shouduo on 2023/6/23.
+//
+
+import SwiftUI
+
+@main
+struct WageTickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
